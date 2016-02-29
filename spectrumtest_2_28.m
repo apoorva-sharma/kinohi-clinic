f42b463717179876c01417fbcaa12b664eab83c2
Apoorva Sharma <me@apoorvasharma.com>
2016-02-28 21:03:03 -0800

    renamed spectrum generator

--- spectrumtest_2_28.m
+++ spectrumtest_2_28.m
@@ -1,4 +1,4 @@
-img = imread('25feb_632.png');
+img = imread('img/2-25 SHS Testing/25feb_sodiumvapor.png');
 img = im2double(img);
 
 figure(1)
@@ -10,16 +10,16 @@ figure(2)
 imagesc(100*log(1+abs(fftshift(F)))); colormap(gray); 
 
 
-Fscaled = 100*log(1+abs(fftshift(F)));
+Fscaled = abs(fftshift(F));
 
-spectrum = Fscaled(465,641:end);
+spectrum = Fscaled(465,:);
 
 figure(3)
 % plot(spectrum);
 % semilogx(spectrum);
 
 % slope is 26 nm/65 pixels = 0.4
-pixel = 0:639;
+pixel = (0:1279) - 640;
 nm = 632+0.4*pixel;
 
 plot(nm,spectrum)
