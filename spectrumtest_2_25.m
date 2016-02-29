9681bfe311a7d0cf56a0bdaae6dd32e358370b34
Erica Martelly <emartelly>
2016-02-28 21:13:10 -0800

    Spectrum 2_28 has a wider band

--- spectrumtest_2_25.m
+++ spectrumtest_2_25.m
@@ -1,4 +1,4 @@
-img = imread('img/28feb_632.png');
+img = imread('img/2-25 SHS Testing/25feb_sodiumvapor.png');
 img = im2double(img);
 
 figure(1)
@@ -12,17 +12,17 @@ imagesc(100*log(1+abs(fftshift(F)))); colormap(gray);
 
 Fscaled = abs(fftshift(F));
 
-spectrum = Fscaled(341,:);
+spectrum = Fscaled(465,:);
 
 figure(3)
 % plot(spectrum);
 % semilogx(spectrum);
 
 % slope is 26 nm/65 pixels = 0.4
-% pixel = (0:1279) - 640;
-% nm = 632+0.4*pixel;
+pixel = (0:1279) - 640;
+nm = 632+0.4*pixel;
 
-% plot(nm,spectrum)
-% xlabel('wavelength (nm)')
-% ylabel('log(intensity)')
-% title('Spectrum')
+plot(nm,spectrum)
+xlabel('wavelength (nm)')
+ylabel('log(intensity)')
+title('Spectrum')
