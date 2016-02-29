d37b69c8142cd2127b66615442c32776d1709b4d
Apoorva Sharma <me@apoorvasharma.com>
2016-02-28 21:54:37 -0800

    updated spectrum script

--- spectrum_script.m
+++ spectrum_script.m
@@ -3,6 +3,20 @@ lambda_d = 632;
 slope = 0.325; %(26nm/80pixels)
 row = 348;
 
-fringe = imread('img/28feb_sodium2.png');
+fringe = imread('img/28feb_658laserpointer2.png');
 
+figure(1)
+[lambda,spectrum] = extractspectrum(fringe,row,slope,lambda_d);
+
+
+
+
+% 2/28 calibration
+lambda_d = 632;
+slope = 0.325; %(26nm/80pixels)
+row = 465;
+
+fringe = imread('img/2-25 SHS Testing/25feb_sodiumvapor.png');
+
+figure(2)
 [lambda,spectrum] = extractspectrum(fringe,row,slope,lambda_d);
