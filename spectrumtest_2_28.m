6e16a2988167968d1d46b1a5b8459ba6d8572e13
Erica Martelly <emartelly>
2016-02-28 21:14:45 -0800

    uncommented the plotting

--- spectrumtest_2_28.m
+++ spectrumtest_2_28.m
@@ -15,14 +15,14 @@ Fscaled = abs(fftshift(F));
 spectrum = Fscaled(341,:);
 
 figure(3)
-% plot(spectrum);
-% semilogx(spectrum);
+plot(spectrum);
+semilogx(spectrum);
 
 % slope is 26 nm/65 pixels = 0.4
-% pixel = (0:1279) - 640;
-% nm = 632+0.4*pixel;
+pixel = (0:1279) - 640;
+nm = 632+0.4*pixel;
 
-% plot(nm,spectrum)
-% xlabel('wavelength (nm)')
-% ylabel('log(intensity)')
-% title('Spectrum')
+plot(nm,spectrum)
+xlabel('wavelength (nm)')
+ylabel('log(intensity)')
+title('Spectrum')
