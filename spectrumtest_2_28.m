dd6793fe3cfb25cc32dc7f328dda4a10143d6912
Erica Martelly <emartelly>
2016-02-28 21:44:03 -0800

    Function that gives the spectrum of a fringe image.

--- spectrumtest_2_28.m
+++ spectrumtest_2_28.m
@@ -12,15 +12,15 @@ imagesc(100*log(1+abs(fftshift(F)))); colormap(gray);
 
 Fscaled = abs(fftshift(F));
 
-spectrum = Fscaled(341,:);
+spectrum = Fscaled(348,:);
 
 figure(3)
 plot(spectrum);
 semilogx(spectrum);
 
-% slope is 26 nm/65 pixels = 0.4
+% slope is 26 nm/80 pixels = 0.325
 pixel = (0:1279) - 640;
-nm = 632+0.4*pixel;
+nm = 632+0.325*pixel;
 
 plot(nm,spectrum)
 xlabel('wavelength (nm)')
