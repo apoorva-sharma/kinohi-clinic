077eb65d0879bd1acec3f03df5330450d4424a60
Apoorva Sharma <kmenda@hmc.edu>
2016-03-28 22:25:40 -0700

    use full paths

--- AveragingCode/process.m
+++ AveragingCode/process.m
@@ -4,14 +4,14 @@ clc
 
 %% Make the averaged Teflon image
 
-filenames = dir('./Teflon_320/');
+filenames = dir('C:\Users\kmenda\Desktop\3_march\Kunal_Raman\321 Tests\Teflon');
 
 TeflonAvg = zeros(2048,2048);
 
 numimages = length(filenames) - 2;
 
 for i = 3:length(filenames)
-    filename = ['./Teflon_320/',filenames(i).name];
+    filename = ['C:\Users\kmenda\Desktop\3_march\Kunal_Raman\321 Tests\Teflon\',filenames(i).name];
     TeflonImg = imread(filename);
     TeflonImg = double(rgb2gray(TeflonImg));
     TeflonAvg = TeflonAvg + TeflonImg/numimages;
@@ -20,14 +20,14 @@ end
 
 %% Make the averaged background image
 
-filenames = dir('./Background_320/');
+filenames = dir('C:\Users\kmenda\Desktop\3_march\Kunal_Raman\321 Tests\Background');
 
 BackgndAvg = zeros(2048,2048);
 
 numimages = length(filenames) - 2;
 
 for i = 3:length(filenames)
-    filename = ['./Background_320/',filenames(i).name];
+    filename = ['C:\Users\kmenda\Desktop\3_march\Kunal_Raman\321 Tests\Background\',filenames(i).name];
     BackgndImg = imread(filename);
     BackgndImg = double(rgb2gray(BackgndImg));
     BackgndAvg = BackgndAvg + BackgndImg/numimages;
